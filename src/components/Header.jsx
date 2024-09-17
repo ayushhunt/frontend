5b4af4ce93b8e3b312a76108f24d57c0204216ab
Abhinavpal320 <apalsonu8875@gmail.com>
2024-09-17 11:49:40 +0530

    Inital header completeion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'; // Optional: for styling
 
 const Header = () => {
   return (
-    <div>
-      <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
-    </div>
-  )
-}
+    <header>
+      <nav className="navbar">
+        <ul className="nav-links">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/signin">Sign In</Link></li>
+          <li><Link to="/signout">Sign Out</Link></li>
+          <li><Link to="/profile" className="profile-button">Profile</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
